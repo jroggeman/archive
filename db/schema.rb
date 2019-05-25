87fff09c1992b5b9ad227e458cd0e4a8d884113f
Joel Roggeman <joelroggeman@gmail.com>
2019-05-24 17:11:40 -0700

    Add document scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_000335) do
+ActiveRecord::Schema.define(version: 2019_05_25_001132) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2019_05_25_000335) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "documents", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_documents_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.text "password_digest"
@@ -42,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_05_25_000335) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "documents", "users"
 end
